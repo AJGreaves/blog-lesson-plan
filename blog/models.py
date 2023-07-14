eb3cfb4fcb2d01221c68195fb7ffc62eec48ea28
AJGreaves <>
2023-07-14 13:33:28 +0000

    Challenge: Build the Comments model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
