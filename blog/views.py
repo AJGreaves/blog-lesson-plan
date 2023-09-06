991a3b4e1b979223b92cb44f52f8e5f8ad7b3346
AJGreaves <>
2023-09-06 11:38:13 +0000

    Add code for templates lesson test deployment

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,16 @@ from .models import Post
 
 
 class PostList(generic.ListView):
+    """
+    This is Django's generic ListView. We are a little limited as to
+    what we can do with this, but if we really needed to, we could
+    access the request object here too.
+
+    It's a class, which is why we had used class-based views in the
+    original blog material. Now, to show what's going on under the
+    hood, so to speak, we'll use function-based ones instead.
+    """
     model = Post
     queryset = Post.objects.filter(status=1)
-    # template = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
