0b0959c1288619cc3132c2fdf72a350632c679a2
AJGreaves <>
2023-09-18 13:46:48 +0000

    3.4b.3 Template inheritance

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,7 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+
+def post_detail(request):
+    return render(request, "blog/post_detail.html")
