e89b985d4d0d5b90aac0b0e1c9704a9c1934d2b3
AJGreaves <>
2023-09-22 09:07:59 +0000

    Views Part 2 lesson code

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from .models import Post
 
@@ -6,11 +6,39 @@ from .models import Post
 
 
 class PostList(generic.ListView):
+    """
+    This is Django's generic ListView. We are a little limited as to
+    what we can do with this, but if we really needed to, we could
+    access the request object here too.
+
+    It's a class, which is why we had used class-based views in the
+    original blog material. Now, to show what's going on under the
+    hood, so to speak, we'll use function-based ones instead.
+    """
+
     model = Post
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
 
-def post_detail(request):
-    return render(request, "blog/post_detail.html")
+def post_detail(request, slug, *args, **kwargs):
+    """
+    A function-based view to view the detail of a post.
+    Largely the same as the class-based, but we don't have
+    different methods for GET and POST. Because it's not a
+    class, all of the extra "self" stuff is removed too.
+
+    Functionally, it's the same, but it is a bit clearer
+    what's going on. To differentiate between request methods,
+    we use request.method == "GET" or request.method == "POST"
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
