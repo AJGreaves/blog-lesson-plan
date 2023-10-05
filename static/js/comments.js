66ba09dd010419fb60709791f843a9653daa8de0
AJGreaves <>
2023-10-05 12:06:01 +0000

    Handle editing comments

---
+++ static/js/comments.js
@@ -0,0 +1,14 @@
+var editButtons = document.getElementsByClassName("btn-edit");
+var commentText = document.getElementsByTagName("textarea")[0];
+var commentForm = document.getElementById("commentForm");
+var submitButton = document.getElementById("submitButton");
+
+for (let button of editButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        let commentContent = document.getElementById(`comment${commentId}`).innerText;
+        commentText.value = commentContent;
+        submitButton.innerText = "Update";
+        commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
