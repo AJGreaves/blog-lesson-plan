e9b7b113075920ab8bb2f05166354adae8ad881b
AJGreaves <>
2023-10-13 09:22:21 +0000

    3.7.3d Handle the form POST request Challenge

--- about/views.py
+++ about/views.py
@@ -9,7 +9,22 @@ def about_me(request, *args, **kwargs):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_request = collaborate_form.save(commit=False)
+            collaborate_request.read = False
+            collaborate_request.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavor to respond within 2 working days.'
+            )
+            collaborate_form = CollaborateForm()
+        else:
+            collaborate_form = CollaborateForm()
+    else:
+        collaborate_form = CollaborateForm()
 
     return render(
         request,
