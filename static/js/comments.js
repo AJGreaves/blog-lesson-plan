572a1f92ad11cf7a0b83fd4b61aa3661188bc745
AJGreaves <>
2023-10-13 09:47:23 +0000

    3.7.4 Editing and deleting records - deleting comments

--- static/js/comments.js
+++ static/js/comments.js
@@ -3,6 +3,10 @@ var commentText = document.getElementById("id_body");
 var commentForm = document.getElementById("commentForm");
 var submitButton = document.getElementById("submitButton");
 
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+var deleteButtons = document.getElementsByClassName("btn-delete");
+var deleteConfirm = document.getElementById("deleteConfirm");
+
 /**
  * Initializes edit functionality for the provided edit buttons.
  * 
@@ -20,5 +24,24 @@ for (let button of editButtons) {
         commentText.value = commentContent;
         submitButton.innerText = "Update";
         commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
+
+
+/**
+ * Initializes deletion functionality for the provided delete buttons.
+ * 
+ * For each button in the `deleteButtons` collection:
+ * - Retrieves the associated comment's ID upon click.
+ * - Updates the `deleteConfirm` link's href to point to the 
+ * deletion endpoint for the specific comment.
+ * - Displays a confirmation modal (`deleteModal`) to prompt 
+ * the user for confirmation before deletion.
+ */
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        deleteConfirm.href = `delete_comment/${commentId}`;
+        deleteModal.show();
     });
 }
