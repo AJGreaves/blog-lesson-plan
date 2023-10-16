f57552a2957224d40d2d41c037da4c9af227c7c8
AJGreaves <>
2023-10-16 12:35:51 +0000

    Adjust tests for blog app, add tests for about app

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import reverse
 from django.test import TestCase
+from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Post
 
 
-class TestViews(TestCase):
+class TestBlogViews(TestCase):
 
     def setUp(self):
         self.user = User.objects.create_superuser(
@@ -13,33 +13,29 @@ class TestViews(TestCase):
                          excerpt="Test excerpt", content="Test content", status=1)
         self.post.save()
 
-    def test_get_post_list(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/index.html')
-
-    def test_get_post(self):
+    def test_render_post_detail_page(self):
         response = self.client.get(reverse('post_detail', args=['test']))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/post_detail.html')
-
-    def test_display_post_details(self):
-        response = self.client.get(reverse('post_detail', args=['test']))
         self.assertIn(b"Test post", response.content)
         self.assertIn(b"Test content", response.content)
 
-    def test_comment_submission(self):
+    def test_successful_comment_submission(self):
         self.client.login(username="test", password="test")
-        response = self.client.post(reverse('post_detail', args=['test']), {
+        post_data = {
             'body': 'This is a test comment.'
-        })
+        }
+        response = self.client.post(
+            reverse('post_detail', args=['test']), post_data)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Comment submitted and awaiting approval',
                       response.content)
-        self.assertTrue(Comment.objects.filter(
-            body="This is a test comment.").exists())
 
-    def test_nonexistent_post(self):
-        response = self.client.get(
-            reverse('post_detail', args=['nonexistent']))
-        self.assertEqual(response.status_code, 404)
+    def test_comment_body_is_required(self):
+        self.client.login(username="test", password="test")
+        post_data = {
+            'body': ''
+        }
+        response = self.client.post(
+            reverse('post_detail', args=['test']), post_data)
+        form = response.context['comment_form']
+        self.assertFalse(form.is_valid())
