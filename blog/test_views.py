870acb7aeea4d7b866fb279c1f5cdbc87546df80
AJGreaves <>
2023-11-06 14:28:34 +0000

    Adjust test_views in blog app

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
 from .models import Post
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.test import TestCase
+from .forms import CommentForm
 
 
 class TestBlogViews(TestCase):
@@ -9,15 +10,17 @@ class TestBlogViews(TestCase):
     def setUp(self):
         self.user = User.objects.create_superuser(
             username="test", password="test", email="test@test.com")
-        self.post = Post(title="Test post", author=self.user, slug="test",
+        self.post = Post(title="Test post", author=self.user, slug="test-post",
                          excerpt="Test excerpt", content="Test content", status=1)
         self.post.save()
 
-    def test_render_post_detail_page(self):
-        response = self.client.get(reverse('post_detail', args=['test']))
+    def test_render_post_detail_page_with_comment_form(self):
+        response = self.client.get(reverse('post_detail', args=['test-post']))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Test post", response.content)
         self.assertIn(b"Test content", response.content)
+        self.assertIsInstance(
+            response.context['comment_form'], CommentForm)
 
     def test_successful_comment_submission(self):
         self.client.login(username="test", password="test")
@@ -25,7 +28,7 @@ class TestBlogViews(TestCase):
             'body': 'This is a test comment.'
         }
         response = self.client.post(
-            reverse('post_detail', args=['test']), post_data)
+            reverse('post_detail', args=['test-post']), post_data)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Comment submitted and awaiting approval',
                       response.content)
