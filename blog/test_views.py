dd4c0920381cab60ef29bc239ac265885f30579e
AJGreaves <>
2023-11-07 10:51:01 +0000

    Testing views sourcecode

--- blog/test_views.py
+++ blog/test_views.py
@@ -10,25 +10,26 @@ class TestBlogViews(TestCase):
     def setUp(self):
         self.user = User.objects.create_superuser(
             username="test", password="test", email="test@test.com")
-        self.post = Post(title="Test post", author=self.user, slug="test-post",
-                         excerpt="Test excerpt", content="Test content", status=1)
+        self.post = Post(title="Blog title", author=self.user,
+                         slug="blog-title", excerpt="Blog excerpt",
+                         content="Blog content", status=1)
         self.post.save()
 
     def test_render_post_detail_page_with_comment_form(self):
-        response = self.client.get(reverse('post_detail', args=['test-post']))
+        response = self.client.get(reverse('post_detail', args=['blog-title']))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Test post", response.content)
-        self.assertIn(b"Test content", response.content)
+        self.assertIn(b"Blog title", response.content)
+        self.assertIn(b"Blog content", response.content)
         self.assertIsInstance(
             response.context['comment_form'], CommentForm)
 
-    # def test_successful_comment_submission(self):
-    #     self.client.login(username="test", password="test")
-    #     post_data = {
-    #         'body': 'This is a test comment.'
-    #     }
-    #     response = self.client.post(
-    #         reverse('post_detail', args=['test']), post_data)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Comment submitted and awaiting approval',
-    #                   response.content)
+    def test_successful_comment_submission(self):
+        self.client.login(username="test", password="test")
+        post_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(
+            reverse('post_detail', args=['blog-title']), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Comment submitted and awaiting approval',
+                      response.content)
